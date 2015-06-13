2e1f5065b46a5ffb49d2ab7070caa142935a6ac7
Jordan Harband <ljharb@gmail.com>
2015-06-13 11:10:13 -0700

    Refactor to use a static regex and `String#replace`.
    This may need to be reverted if there is ever a `RegExp` syntax character that is a code point, or half of a surrogate pair. (relates to https://github.com/benjamingr/RexExp.escape/issues/10)

--- index.js
+++ index.js
@@ -2,28 +2,12 @@
 
 var define = require('define-properties');
 var ES = require('es-abstract/es7');
-var at = require('string-at');
 var bind = require('function-bind');
-var join = bind.call(Function.call, Array.prototype.join);
-
-var isSyntaxCharacter = function isSyntaxCharacter(c) {
-	return c.length === 1 && '^$\\.*+?()[]{}|'.indexOf(c) > -1;
-};
+var replace = bind.call(Function.call, String.prototype.replace);
+var syntaxChars = /[\^\$\\\.\*\+\?\(\)\[\]\{\}\|]/g;
 
 var escapeShim = function escape(S) {
-	var str = ES.ToString(S);
-	var cpListLength = 0;
-	var codePoint;
-	var cuList = [];
-	while (cpListLength < str.length) {
-		codePoint = at(str, cpListLength);
-		cpListLength += codePoint.length;
-		if (isSyntaxCharacter(codePoint)) {
-			cuList.push('\\');
-		}
-		cuList.push(codePoint);
-	}
-	return join(cuList, '');
+	return replace(ES.ToString(S), syntaxChars, '\\$&');
 };
 
 define(escapeShim, {
