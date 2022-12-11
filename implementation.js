612e7bd3c29ff4070dfff0ae1b101d7d86b643dc
Jordan Harband <ljharb@gmail.com>
2022-12-11 10:17:01 +0000

    [Deps] update `define-properties`, `es-abstract`; add `call-bind`

--- implementation.js
+++ implementation.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var ToString = require('es-abstract/2019/ToString');
-var callBound = require('es-abstract/helpers/callBound');
+var ToString = require('es-abstract/2022/ToString');
+var callBound = require('call-bind/callBound');
 var $replace = callBound('String.prototype.replace');
 var syntaxChars = /[\^$\\.*+?()[\]{}|]/g;
 
