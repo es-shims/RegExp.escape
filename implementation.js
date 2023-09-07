e661afa82416733cb5e41e089f944514819efa98
Jordan Harband <ljharb@gmail.com>
2023-09-06 22:11:50 -0700

    [Deps] update `es-abstract`

--- implementation.js
+++ implementation.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var ToString = require('es-abstract/2022/ToString');
+var ToString = require('es-abstract/2023/ToString');
 var callBound = require('call-bind/callBound');
 var $replace = callBound('String.prototype.replace');
 var syntaxChars = /[\^$\\.*+?()[\]{}|]/g;
