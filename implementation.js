a0d43907fe52b950de016b3765f5591766388468
Jordan Harband <ljharb@gmail.com>
2023-10-19 11:53:21 -0700

    [Refactor] update spec to match proposal
    This escapes more characters, but is not considered breaking because the purpose of the API remains the same: using the output in `new RegExp`

--- implementation.js
+++ implementation.js
@@ -1,10 +1,42 @@
 'use strict';
 
+// var CodePointsToString = require('es-abstract/2023/CodePointsToString');
 var ToString = require('es-abstract/2023/ToString');
+var StringToCodePoints = require('es-abstract/2023/StringToCodePoints');
+// var UTF16EncodeCodePoint = require('es-abstract/2023/UTF16EncodeCodePoint');
+var regexTester = require('safe-regex-test');
+var forEach = require('for-each');
+
 var callBound = require('call-bind/callBound');
-var $replace = callBound('String.prototype.replace');
-var syntaxChars = /[\^$\\.*+?()[\]{}|]/g;
+
+var $indexOf = callBound('Array.prototype.indexOf');
+var $join = callBound('Array.prototype.join');
+var $push = callBound('Array.prototype.push');
+
+var asciiPunctuators = "(){}[]|,.?*+-^$=<>\\/#&!%:;@~'\"`";
+
+var isDecimalDigit = regexTester(/^\d$/);
+var isWhiteSpace = regexTester(/^\s$/);
 
 module.exports = function escape(S) {
-	return $replace(ToString(S), syntaxChars, '\\$&');
+	var str = ToString(S); // step 1
+
+	var cpList = StringToCodePoints(str); // step 2
+
+	var toEscape = StringToCodePoints(asciiPunctuators); // step 3
+
+	var escapedList = []; // step 4
+
+	forEach(cpList, function (c) { // step 5
+		if (escapedList.length === 0 && isDecimalDigit(c)) { // step 5.a
+			$push(escapedList, '\u005c', '\u0078', '\u0033'); // step 5.a.i - 5.a.iii
+		} else if ($indexOf(toEscape, c) > -1 || isWhiteSpace(c)) { // step 5.b
+			$push(escapedList, '\u005c'); // step 5.b.i
+		}
+		// c. Append the elements of UTF16EncodeCodePoint(c) to escapedList.
+		$push(escapedList, c);
+	});
+
+	// return CodePointsToString(escapedList); // step 6
+	return $join(escapedList, '');
 };
